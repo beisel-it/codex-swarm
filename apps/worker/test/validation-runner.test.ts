4cae3ce1b6910e7d71e0ede10498dccfc7c73431
Florian Beisel <florian@beisel.org>
2026-04-02 21:32:16 +0200

    test: align worker DAG fixtures with contract

--- apps/worker/test/validation-runner.test.ts
+++ apps/worker/test/validation-runner.test.ts
@@ -113,7 +113,9 @@ const runDetail: RunDetail = {
     edges: [],
     rootTaskIds: ["33333333-3333-4333-8333-333333333333"],
     blockedTaskIds: [],
-    unblockPaths: []
+    unblockPaths: [],
+    hasIncompleteDependencies: false,
+    missingDependencies: []
   }
 };
 
